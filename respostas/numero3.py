6db71a5604e766a7f3cdc9545c94a06aa29d1e31
EmilaElyson <emiliaelyson@gmail.com>
2024-04-09 20:49:19 -0300

    adicionando atividade3

---
+++ respostas/numero3.py
@@ -0,0 +1,10 @@
+    #Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+def calculo_tres_numeros(num1, num2, num3):
+    soma = (num1 + num2 + num3)
+    print(soma)
+
+num1, num2, num3 = 9, 8, 5
+calculo_tres_numeros(num1, num2, num3)
+
+
