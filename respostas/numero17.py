d4866b26d1d9260fc579667c109e4cdf76cc2341
EmilaElyson <emiliaelyson@gmail.com>
2024-04-09 20:56:31 -0300

    adicionando atividade17

--- respostas/numero17.py
+++ respostas/numero17.py
@@ -12,19 +12,19 @@ preco_galao = 25
     # apenas latas de 18 litros
 texto = "Comprando apenas latas de 18 litros: "
 
-quantidade_de_tinta_comprada = int(metros_quadrados_area_pintada) * cobertura_metro_por_litro
 quantidade_de_latas = int(metros_quadrados_area_pintada) / litros_lata
-preco_latas = preco_por_lata * quantidade_de_tinta_comprada
-print("A quantidade de latar compradas e o preço total delas é de: " + str(quantidade_de_tinta_comprada) + " litros e " + str(preco_latas) + " reais ")
+preco_latas = preco_por_lata * quantidade_de_latas
+print("A quantidade de latas compradas e o preço total delas é de: " + str(quantidade_de_latas) + " latas e " + str(preco_latas) + " reais ")
 
     # segunda situação
     # apenas galões de 3,6 litros
 texto = "Comprando apenas galões de 3,6 litros: "
 quantidade_galoes_comprados = int(metros_quadrados_area_pintada) / litros_galao
-preco_galoes = preco_galao * metros_quadrados_area_pintada
-print ("A quantidade de galões comprados e o preço total deles é de: " + str(quantidade_galoes_comprados) + " e " + str(preco_galoes))
+preco_galoes = preco_galao * quantidade_galoes_comprados
+print ("A quantidade de galões comprados e o preço total deles é de: " + str(quantidade_galoes_comprados) + " galões e " + str(preco_galoes) + " reais ")
 
 
     #terceira situação
     # misturando latas e galões
 texto = "Misturando latas e galões"
+    # Não consegui responder pela falta de tempo do final do dia.
