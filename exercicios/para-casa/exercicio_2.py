f7fa754e06d11fb81280a1025bbcc7a0c28c6dbf
Fernanda Añez <fernanda.anez@gmail.com>
2024-04-09 19:06:52 -0300

    Alteração no exercício 2

--- exercicios/para-casa/exercicio_2.py
+++ exercicios/para-casa/exercicio_2.py
@@ -8,7 +8,6 @@ valor_lata = 80
 quantidade_galao = 3.6
 valor_galao = 25
 
-#Cálculo por lata
 area_lata = rendimento_litro * quantidade_lata
 quantidade_total_latas = tamanho_da_area / area_lata
 total_lata = math.ceil(quantidade_total_latas) * valor_lata
@@ -16,7 +15,6 @@ print('OPÇÃO 1: COMPRANDO APENAS LATAS')
 print('Considerando apenas latas, você deverá comprar ', math.ceil(quantidade_total_latas), 'unidades.')
 print('Valor total - apenas latas: R$', total_lata)
 
-#Cálculo por galão
 area_galao = rendimento_litro * quantidade_galao
 quantidade_total_galoes = tamanho_da_area / area_galao
 total_galao = math.ceil(quantidade_total_galoes) * valor_galao
@@ -25,7 +23,6 @@ print('Considerando apenas galões, você deverá comprar ', math.ceil(quantidad
 print('Valor total - apenas galões: R$', total_galao)
 
 
-#Cálculo da quantidade de latas para pintar tamanho_da_area
 print('OPÇÃO 3: COMPRANDO LATAS E GALÕES')
 tamanho_da_area_mistura = tamanho_da_area * 1.1
 mistura_lata = tamanho_da_area_mistura//area_lata
@@ -34,5 +31,6 @@ mistura_galao = (tamanho_da_area_mistura % area_lata)/area_galao
 print('Quantidade de galões na mistura', math.ceil(mistura_galao))
 total_mistura = (math.ceil(mistura_lata) * valor_lata) + (math.ceil(mistura_galao) * valor_galao)
 print('Valor total - latas e galões: R$', round(total_mistura, 2))
+
 print('Entre em contato com nossos vendedores para conhecer nossas formas de pagamento!')
 
