6dc58022fb3e25c0c5c1117744873f5e2898a2f1
lucianacarvl <luccarvl@gmail.com>
2024-04-09 22:26:17 -0300

    Adicionando arquivos

---
+++ exercicios/para-casa/questao3.py
@@ -0,0 +1,8 @@
+# Questão 3
+def soma(a1, a2, a3):
+  return a1 + a2 + a3
+a1 = input("Insira uma letra, palavra ou número:")
+a2 = input("Insira uma letra, palavra ou número:")
+a3 = input("Insira uma letra, palavra ou número:")
+concat = soma(a1, a2, a3)
+print("A soma dos três é:", concat)
